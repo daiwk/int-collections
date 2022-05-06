018981ec76f685c7ca8cecc52fa95c86c18de8db
daiwk <daiwk@foxmail.com>
2022-05-07 03:00:53 +0800

    xx

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -1,3 +1,4 @@
 
-pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./int.md -o ./int.pdf
+#pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./int.md -o ./int.pdf
+pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./int-summary.md -o ./int-summary.pdf
 
